70bddc25d36fb3ab45ec5ab250de3ec4ed8e9c59
Justin Bailey <justin.bailey@urbanairship.com>
2014-07-17 10:00:00 -0700

    Multiple operations with fixed result type.

--- src/OpenFile.hs
+++ src/OpenFile.hs
@@ -6,32 +6,54 @@ import System.FilePath (FilePath)
 import Data.Typeable
 import qualified Data.Char as Char
 
-newtype OpenFile a v = Read (a -> v)
+data OpenFile a v = Read (Result -> v) | EOF (Result -> v)
   deriving (Typeable, Functor)
 
-readOpenFile :: (Member (OpenFile Char) r) => Eff r Char
-readOpenFile = send $ \k -> inj (Read k)
+data Result = C Char | F Bool
+  deriving (Typeable) 
 
-runOpenFile :: FilePath -> Eff (OpenFile Char :> ()) result -> IO result
-runOpenFile path action = do 
+-- | Read a character from an open file.
+readChar :: (Member (OpenFile Result) r) => Eff r Result
+readChar = send $ \k -> inj (Read k `asTypeOf` (toOF k))
+
+-- | Check if we are at EOF.
+atEOF :: (Member (OpenFile Result) r) => Eff r Result
+atEOF = send $ \k -> inj (EOF k `asTypeOf` (toOF k))
+
+-- | A helper function to fix the type of "OpenFile a" in the above
+-- request functions.
+toOF :: (a -> v) -> OpenFile a v
+toOF = error "toOF should never be evaulated"
+
+-- | Run a program with the OpenFile effect.
+runOpenFile :: FilePath -> Eff (OpenFile Result :> r) result -> IO result
+runOpenFile path action = do
     h <- IO.openFile path ReadMode
     loop h (admin action)
 
-loop :: Handle -> (VE (OpenFile Char :> ()) result) -> IO result
+loop :: (Typeable a) => Handle -> (VE (OpenFile a :> r) result) -> IO result
 loop h (Val x) = return x
-loop h (E u) = prjForce u (doRead h)
-
-doRead :: Handle -> OpenFile Char (VE (OpenFile Char :> ()) result) -> IO result
-doRead h (Read k) = do 
-  c <- hGetChar h
-  loop h (k c)
-
+loop h eff@(E u) = 
+  let -- Helper function to fix the type
+      -- of the result of `prj`.
+      getVE :: Union (OpenFile a :> r) (VE ((OpenFile a) :> r) result) -> Maybe (OpenFile a (VE ((OpenFile a) :> r) result))
+      getVE eff = error "getVE should never be evaluated"undefined
+  in case prj u `asTypeOf` getVE u of
+       Just (Read k) -> do
+         char <- hGetChar h
+         loop h (k (C char))
+       Just (EOF k) -> do 
+         eof <- hIsEOF h
+         loop h (k (F eof))
+       Nothing -> error "Could not find OpenFile"
+ 
 main = do
   x <- runOpenFile "Setup.hs" (do
-    x <- readOpenFile 
-    y <- readOpenFile   
-    return [x,y])
+    (C x) <- readChar
+    (F eof) <- atEOF
+    (C y) <- readChar
+    return $ [x,y] ++ show eof)
   putStrLn (show x)
-  x <- runOpenFile "Setup.hs" readOpenFile :: IO Char
+  (C x) <- runOpenFile "Setup.hs" readChar
   putStrLn (show x)
   
